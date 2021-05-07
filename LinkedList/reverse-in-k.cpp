4a6389da013b2edfec0bb34478a7df59bbeec372
Mighty-Geek <aamishajangra@gmail.com>
2021-05-07 21:15:01 +0530

    Add reverse-in-k.cpp

--- LinkedList/reverse-in-k.cpp
+++ LinkedList/reverse-in-k.cpp
@@ -38,16 +38,35 @@ void printList(Node *head)
     cout << head->data;
 }
 
-Node *reverseKGroup(Node *head, int k)
+Node *reverseKGroup(Node *&head, int k)
 {
-    return head;
+    Node *prev_ptr = NULL;
+    Node *curr_ptr = head;
+    Node *next_ptr;
+
+    int count = 0;
+    while (curr_ptr != NULL && count < k)
+    {
+        next_ptr = curr_ptr->next;
+        curr_ptr->next = prev_ptr;
+        prev_ptr = curr_ptr;
+        curr_ptr = next_ptr;
+        count++;
+    }
+
+    if (next_ptr != NULL)
+    {
+        head->next = reverseKGroup(next_ptr, k);
+    }
+
+    return prev_ptr;
 }
 
 int main()
 {
     Node *head = new Node(1);
     Node *second = new Node(2);
-    Node *third = new Node(3);
+    Node *third = new Node(2);
 
     head->next = second;
     second->next = third;
@@ -56,10 +75,10 @@ int main()
     appendList(head, 4);
     appendList(head, 5);
     appendList(head, 6);
+    appendList(head, 7);
+    appendList(head, 8);
 
-    int k;
-    cout << "Enter size of groups(k) : ";
-    cin >> k;
+    int k = 4;
     Node *res = reverseKGroup(head, k);
 
     printList(res);
