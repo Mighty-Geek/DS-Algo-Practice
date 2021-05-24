6c6c823b5007ea63537ee00f2763756e0a2bc7ee
Mighty-Geek <aamishajangra@gmail.com>
2021-05-24 20:43:06 +0530

    Add smallest-window-1.cpp

--- Hashmap/smallest-window-1.cpp
+++ Hashmap/smallest-window-1.cpp
@@ -2,6 +2,10 @@
 #include <climits>
 using namespace std;
 
+// Solution using Hashmap
+// Time complexity: O(m+n)
+// Auxiliary space: O(m+n)
+/*
 const int no_of_chars = 256;
 
 string findSubstr(string str, string pat)
@@ -62,6 +66,52 @@ string findSubstr(string str, string pat)
     }
     cout << "Minimum length : " << min_len;
     return str.substr(start_index, min_len);
+}
+*/
+// Solution using sliding window
+string findSubstr(string s, string t)
+{
+    int m[256] = {0};
+
+    int ans = INT_MAX;
+    int start = 0;
+    int count = 0;
+
+    for (int i = 0; i < t.length(); i++)
+    {
+        if (m[t[i]] == 0)
+            count++;
+        m[t[i]]++;
+    }
+
+    int i = 0, j = 0;
+    while (j < s.length())
+    {
+        m[s[j]]--;
+        if (m[s[j]] == 0)
+            count--;
+        if (count == 0)
+        {
+            while (count == 0)
+            {
+                if (ans > j - i + 1)
+                {
+                    ans = min(ans, j - i + 1);
+                    start = i;
+                }
+                m[s[i]]++;
+                if (m[s[i]] > 0)
+                    count++;
+
+                i++;
+            }
+        }
+        j++;
+    }
+    if (ans != INT_MAX)
+        return s.substr(start, ans);
+    else
+        return "-1";
 }
 
 int main()
