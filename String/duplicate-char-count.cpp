02fd021b7f2be77b4e8fbd233196e7b029d201e4
Mighty-Geek <aamishajangra@gmail.com>
2021-06-05 03:31:40 +0530

    Updated duplicate-char-count.cpp

--- String/duplicate-char-count.cpp
+++ String/duplicate-char-count.cpp
@@ -1,1 +1,28 @@
+// using Hashing
 
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+void duplicateCharCount(string s)
+{
+    int n = s.length();
+    unordered_map<char, int> count;
+    for (int i = 0; i < n; i++)
+    {
+        count[s[i]]++;
+    }
+    for (auto x : count)
+    {
+        if (x.second > 1)
+            cout << "Count of \'" << x.first << "\' is " << x.second << endl;
+    }
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+    duplicateCharCount(str);
+    return 0;
+}
